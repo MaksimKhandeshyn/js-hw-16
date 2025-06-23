5b32b53c3e12d19d1fae277c0f068419a8074367
Maksim Khandeshin <maksimhandeshin@gmail.com>
2025-06-23 21:32:39 +0300

    Will make after saturday

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import { countries } from "./js/fetchCountries";
-const input = document.querySelector(".input-js");
-
-const inptuVal = input.value;
-console.log(inptuVal);
+countries();
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,17 @@
-export const countries = (fetchCountries = (searchQuery) => {
+export function countries() {
   const input = document.querySelector(".input-js");
-
-  const inptuVal = input.value;
-  console.log(inptuVal);
-});
+  input.addEventListener("input", onInputClick);
+  const COUNTRIES = "https://restcountries.com/v3.1/all?fields=name";
+  function onInputClick() {
+    fetch(COUNTRIES)
+      .then((value) => {
+        console.log(value);
+        console.log(value.body);
+        console.log(value.json());
+        const COUNTRY = `https://restcountries.com/v3.1/name/${value}`;
+        console.log(COUNTRY);
+        // console.log(value);
+      })
+      .catch(console.log(`Error`));
+  }
+}
